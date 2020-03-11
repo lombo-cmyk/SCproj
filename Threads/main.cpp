d24a411e8361db27d045af1157bdae30e5b8bcd5
lukaszk <lukasz.kania.cc@gmail.com>
2020-03-11 17:03:27 +0100

    Draft for whole Threads xercise, added 1.3 - value increment

--- Threads/main.cpp
+++ Threads/main.cpp
@@ -2,9 +2,10 @@
 #include <thread>
 #include <mutex>
 #include <vector>
-
+#include <atomic>
 static std::mutex locker;
-
+std::atomic_int GlobalIntValue = 0;
+//int GlobalIntValue = 0;
 void FunctionThreadInfo(std::string s){
         std::cout << s << "my id is:" <<std::this_thread::get_id();
 }
@@ -21,6 +22,7 @@ struct ThreadStruct{
         std::cout << s << "my id is:" <<std::this_thread::get_id();
     }
 };
+
 void Print50Times(std::string s){
     for (int i=0; i<50; i++){
         locker.lock();
@@ -30,7 +32,20 @@ void Print50Times(std::string s){
     }
 }
 
+void IncrementValue(){
+    auto start=std::chrono::system_clock::now();
+    for (int i=0; i<10000000; i++){
+        locker.lock();
+        GlobalIntValue++;
+        locker.unlock();
+    }
+    auto stop=std::chrono::system_clock::now();
+    std::chrono::duration<double> duration=stop-start;
+    std::cout << "Execution time for "<< std::this_thread::get_id() <<" is: "<< duration.count() << std::endl;
+}
+
 int main() {
+    /*Task one*/
     std::thread t1(FunctionThreadInfo,"\nI'm thread 1, ");
     std::thread t2(ThreadClass::ClassMethod,"\nI'm thread 2, ");
     std::thread t3(ThreadStruct(),"\nI'm thread 3, ");
@@ -41,6 +56,8 @@ int main() {
     t2.join();
     t3.join();
     t4.join();
+
+    /*Task 2*/
     std::thread PrintTh[20];
     for(int j=0; j<20; j++){
         PrintTh[j]=std::thread(Print50Times, ("Thread "+ std::to_string(j)));
@@ -48,5 +65,17 @@ int main() {
     for(int j=0; j<20; j++){
         PrintTh[j].join();
     }
+
+    /*Task 3*/
+    std::vector<std::thread> VectorOfThreads;
+    for( int i=0; i<10; i++){
+        VectorOfThreads.emplace_back(IncrementValue);
+    }
+    for( int i=0; i<10; i++){
+        VectorOfThreads[i].join();
+    }
+
+    IncrementValue();
+
     return 0;
 }
