500dfc7c555143b21f4064bba8bfdfa6d4137c3c
lukaszk <lukasz.kania.cc@gmail.com>
2020-03-11 11:43:26 +0100

    Project Start

---
+++ cmake-build-debug/CMakeFiles/SC_proj_threads.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SC_proj_threads.dir/main.cpp.obj"
+  "SC_proj_threads.exe"
+  "SC_proj_threads.exe.manifest"
+  "SC_proj_threads.pdb"
+  "libSC_proj_threads.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SC_proj_threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
