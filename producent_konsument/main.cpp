289ed14e1f94081c8383244373e28624ae3e9851
lukaszk <lukasz.kania.cc@gmail.com>
2020-03-13 22:56:08 +0100

    Properly working q and producer

--- producent_konsument/main.cpp
+++ producent_konsument/main.cpp
@@ -2,51 +2,40 @@
 #include <array>
 #include <vector>
 #include <random>
+#include <memory>
 
-
-#define ARRSIZE 100000
+#define ARRSIZE 10
+#define QUEUELENGHT 10
 
 class Queue{
 public:
     int q_lenght;
-    static std::vector<std::array<int, ARRSIZE>> queue_vector;
+    std::vector<std::array<int, ARRSIZE>> queue_vector;
     //std::vector<int> queue_vector;
     Queue(int queue_lenght){
         q_lenght=queue_lenght;
     }
-//    void add_element(std::array<int, ARRSIZE> new_element){
-//        if(queue_vector.size()<q_lenght){
-//            queue_vector.push_back(new_element);
-//        }
-//    }
-//
-//    std::array<int, ARRSIZE> take_element(){
-//        std::array<int, ARRSIZE> ret_val=queue_vector.front();
-//        queue_vector.erase(queue_vector.begin());
-//        return ret_val;
-//    }
-
+    
 };
 
 class Producer{
 public:
-    std::vector<std::array<int, ARRSIZE>> queue;
-    int number_of_arrays;
+    std::shared_ptr<Queue> queue;
+    int number_of_elements;
 
-    Producer(std::vector<std::array<int, ARRSIZE>> passed_queue, int passed_number_of_arrays){
+    Producer(std::shared_ptr<Queue> passed_queue, int passed_number_of_elements){
         queue=passed_queue;
-        number_of_arrays=passed_number_of_arrays;
+        number_of_elements=passed_number_of_elements;
     }
     void adding_elements_to_queue(){
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(1, 50000);
+        std::uniform_int_distribution<> dis(-50000, 50000);
         std::array<int, ARRSIZE> queue_element;
-        for (int i=0; i<number_of_arrays; i++){
+        for (int i=0; i<number_of_elements; i++){
             queue_element[i]=dis(gen);
         }
-        queue.push_back(queue_element);
-        Queue::queue_vector.push_back(queue_element);
+        queue.get()->queue_vector.push_back(queue_element);
     }
 };
 
@@ -54,14 +43,12 @@ public:
 
 int main() {
 
-    Queue my_queue(10);
-    Producer my_producer(Queue::queue_vector, 1000);
-
-
+    auto my_queue=std::make_shared<Queue>(QUEUELENGHT);
+    Producer my_producer(my_queue, 10);
+    my_producer.adding_elements_to_queue();
     std::array<int, ARRSIZE> queue_element{};
-    queue_element[0]=1;
-    queue_element[1]=2;
-    queue_element[2]=3;
-    std::cout << queue_element[0] << queue_element[1] << queue_element[2] << std::endl;
+    for(int i=0; i<10; i++){
+        std::cout << my_queue.get()->queue_vector[0][i] << std::endl;
+    }
 
 }
