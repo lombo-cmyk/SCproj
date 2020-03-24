a638e143450d2c666d707932bb222605d82b732a
lukaszk <lukasz.kania.cc@gmail.com>
2020-03-24 12:28:36 +0100

    Clean global variables

--- producent_konsument/main.cpp
+++ producent_konsument/main.cpp
@@ -10,19 +10,21 @@
 #include <atomic>
 #include <condition_variable>
 
+//TODO Report
+
 #define ARR_SIZE 100000
 #define QUEUE_LENGTH 200
 #define NUMBER_OF_ARRAYS 4000
 
-std::condition_variable conditionVariable;
-std::mutex locker;
-std::mutex print_mutex;
-std::atomic<int> numberOfConsumedArrays;
 
 class Queue{
 public:
     int m_length;
     std::vector<std::array<int, ARR_SIZE>> m_queueVector;
+    std::mutex m_queMutex;
+    std::mutex m_printMutex;
+    std::condition_variable m_conditionVariable;
+    std::atomic<int> m_numberOfConsumedArrays = 0;
     explicit Queue(int queue_length) {
         m_length=queue_length;
     }
@@ -45,24 +47,26 @@ public:
         int j=0;
         std::array<int, ARR_SIZE> queueElement={};
         while(j < m_numberOfArrays) {
-            if(m_queue->m_queueVector.size() < QUEUE_LENGTH){
+            if(m_queue->m_queueVector.size() < m_queue->m_length){
                 j++;
                 for (int i = 0; i < ARR_SIZE; i++) {
                     queueElement[i] = dis(gen);
                 }
                 {
-                    std::lock_guard<std::mutex> lock(locker);
+                    std::lock_guard<std::mutex> lock(m_queue->m_queMutex);
                     m_queue->m_queueVector.push_back(queueElement);
                 }
-                conditionVariable.notify_one();
+//                conditionVariable.notify_one();
+                m_queue->m_conditionVariable.notify_one();
             }
             else{
-                conditionVariable.notify_one();
+//                conditionVariable.notify_one();
+                m_queue->m_conditionVariable.notify_one();
                 std::this_thread::yield();
             }
 
         }
-        std::lock_guard<std::mutex> lock(print_mutex);
+        std::lock_guard<std::mutex> lock(m_queue->m_printMutex);
         std::cout << "PRODUCER FINISHED" << std::endl;
     }
 };
@@ -80,25 +84,28 @@ public:
         int numberOfConsumedArraysByMe=0;
         std::array<int, ARR_SIZE> queueElement={};
         while(true){
-            std::unique_lock<std::mutex> lck(locker);
+            std::unique_lock<std::mutex> lck(m_queue->m_queMutex);
             if (!m_queue->m_queueVector.empty()) {
                 queueElement = m_queue->m_queueVector.front();
                 m_queue->m_queueVector.erase(m_queue->m_queueVector.begin());
                 lck.unlock();
-                numberOfConsumedArrays++;
+                m_queue->m_numberOfConsumedArrays++;
                 numberOfConsumedArraysByMe++;
                 std::sort(queueElement.begin(), queueElement.end());
             }
             else {
                 std::this_thread::yield();
-                if(conditionVariable.wait_for(lck,std::chrono::seconds(1))==std::cv_status::timeout) break;
+                if(m_queue->m_conditionVariable.wait_for(lck,std::chrono::seconds(1))==std::cv_status::timeout) break;
             }
         }
-        std::lock_guard<std::mutex> lock(print_mutex);
+        std::lock_guard<std::mutex> lock(m_queue->m_printMutex);
         std::cout << "I consumed: " << numberOfConsumedArraysByMe << " arrays" << std::endl;
     }
 };
-
+int user_input(int input){
+    const int  my_inp=input;
+    return my_inp;
+}
 
 int main() {
     /*8 concurrent threads are supported.*/
@@ -108,7 +115,7 @@ int main() {
     auto myQueue=std::make_shared<Queue>(QUEUE_LENGTH);
     Producer myProducer(myQueue, NUMBER_OF_ARRAYS);
 
-    const std::size_t NumberOfThreads=7;
+    const std::size_t NumberOfThreads=5;
     std::array<Consumer, NumberOfThreads> myConsumers={};
     for (auto& consumer : myConsumers){
         consumer=myQueue;
@@ -125,5 +132,6 @@ int main() {
     std::chrono::duration<double> duration=stop-start;
 
     std::cout << "Program finished in: "<< duration.count() << std::endl;
+    std::cout << "No all consumed arrays <atomic>: " << myQueue->m_numberOfConsumedArrays << std::endl;
     system("Pause");
 }
